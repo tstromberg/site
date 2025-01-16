c5aeb655d6cfde0b9b6dfd4b0cbf373a8ac896a6
Thomas Stromberg <t+github@chainguard.dev>
2025-01-16 06:07:30 -0500

    logs and tweaks

--- functions/t/p/[[all]].js
+++ functions/t/p/[[all]].js
@@ -4,6 +4,8 @@ export async function onRequestGet(ctx) {
     path = path + "index.html";
   }
   const file = await ctx.env.MEDIA.get(path);
+  console.log(`URL ${context.request.url} became path: ${path}`);
+
   if (!file) return new Response(null, { status: 404 });
   return new Response(file.body, {
     headers: { "Content-Type": file.httpMetadata.contentType },
