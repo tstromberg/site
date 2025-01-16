1038b40d3d85bd0110db03b81abfe3a8413e9454
Thomas Stromberg <t+github@chainguard.dev>
2025-01-16 06:19:20 -0500

    log trailing /

--- functions/t/p/[[all]].js
+++ functions/t/p/[[all]].js
@@ -1,8 +1,16 @@
 export async function onRequestGet(ctx) {
   var path = new URL(ctx.request.url).pathname.replace("/t/p/", "");
+
+  if (path == "") {
+    console.log("${path} is empty");
+    path = "/index.html";
+  }
+
   if (path.endsWith("/")) {
+    console.log("${path} ends with a slash");
     path = path + "index.html";
   }
+
   const file = await ctx.env.MEDIA.get(path);
   console.log(`URL ${ctx.request.url} became path: ${path}`);
 
